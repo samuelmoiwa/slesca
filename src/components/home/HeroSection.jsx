2f21b46e14c38c3dd0f8281e1f823147936b285e
Moiwa <samuelmoiwa@gmail.com>
2025-01-07 22:33:12 +0000

    update

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
-    <div className=' text-red-400'>HeroSection</div>
+    <div className=' text-red-400 text-5xl'>HeroSection</div>
   )
 }
 
