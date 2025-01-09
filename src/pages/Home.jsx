10111ad10459d81e3c4a17406964d552ebdcd470
Moiwa <samuelmoiwa@gmail.com>
2025-01-09 11:43:24 +0000

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,9 @@ import HeroSection from '../components/home/HeroSection'
 
 const Home = () => {
   return (
-    <>
+    <div className='min-h-screen'>
     <HeroSection />
-    </>
+    </div>
   )
 }
 
