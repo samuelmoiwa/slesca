9e7fd6f818750eed26b7fe4b72fd1d5848ed40f4
Moiwa <samuelmoiwa@gmail.com>
2025-01-09 14:50:39 +0000

    update

--- src/components/home/AboutNote.jsx
+++ src/components/home/AboutNote.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import missionImage from '../../image/8.jpg'
+import missionImage from '../../image/9.jpg'
 
 const AboutNote = () => {
   return (
-    <div className="w-full max-w-[80rem] px-4 py-12 bg-green-100 shadow mt-[10rem]">
+    <div className="w-full max-w-[80rem] px-4 py-6 bg-green-100 shadow mt-[7rem]">
       <div className="flex flex-col md:flex-row items-center md:space-x-8">
         {/* Text Section */}
         <div className="md:w-1/2">
@@ -13,14 +13,6 @@ const AboutNote = () => {
           </p>
 
           <h3 className="text-2xl font-semibold text-green-600 mt-6 mb-2">Our Mission</h3>
-          <p className="text-gray-700 mb-4">
-            To build a robust and sustainable seed sector that guarantees seed sufficiency, contributing to food security in Sierra Leone.
-          </p>
-
-          <h3 className="text-2xl font-semibold text-green-600 mt-6 mb-2">Our Vision</h3>
-          <p className="text-gray-700 mb-4">
-            To establish a sustainable seed system, reducing seed importation and making the seed sector attractive for private sector investment.
-          </p>
 
           <a
             href="/about"
