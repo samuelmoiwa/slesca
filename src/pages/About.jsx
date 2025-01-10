4b18b064123f9f917264ffec2e4acb03966587e3
Moiwa <samuelmoiwa@gmail.com>
2025-01-10 17:14:25 +0000

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -14,8 +14,7 @@ import TestimonialsSection from '../components/about/TestimonialsSection'
 const About = () => {
   return (
     <>
-
-    <div className='bg-gradient-to-b from-blue-50 to-green-200 w-screen h-full flex justify-center items-center flex-col'>
+      <div className='bg-gradient-to-b from-blue-50 to-green-200 w-screen h-full flex justify-center items-center flex-col'>
           <HeroSection/>
         <div className=' w-full py-14 flex flex-col justify-center
           items-center mt-5 mb-10'
@@ -32,9 +31,6 @@ const About = () => {
       <WhySLeSCAMatters/>
       <LeadershipAndTeam/>
       <TestimonialsSection/>
-
-
-
     </>
   )
 }
