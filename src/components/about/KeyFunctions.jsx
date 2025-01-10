168116de981ddeef4a21c079169e0b2636318f5e
Moiwa <samuelmoiwa@gmail.com>
2025-01-10 11:20:37 +0000

    update

--- src/components/about/KeyFunctions.jsx
+++ src/components/about/KeyFunctions.jsx
@@ -151,22 +151,25 @@ const SkeletonTwo = () => {
     },
 
   };
-
+  const arr = new Array(6).fill(0);
   return (
     <motion.div
-      initial="initial"
-      animate="animate"
-      whileHover="hover"
-      className="flex flex-1 w-full h-full min-h-[6rem] dark:bg-dot-white/[0.2] bg-dot-black/[0.2] flex-col space-y-2"
-    >
-      {[...Array(6)].map((_, i) => (
-        <motion.div
-          key={"skeleton-two-" + i}
-          variants={variants}
-          className="w-full h-4 bg-gray-200 rounded-full"
-        />
-      ))}
-    </motion.div>
+    initial="initial"
+    animate="animate"
+    whileHover="hover"
+    className="flex flex-1 w-full h-full min-h-[6rem] dark:bg-dot-white/[0.2] bg-dot-black/[0.2] flex-col space-y-2"
+  >
+    {arr.map((_, i) => (
+      <motion.div
+        key={"skelenton-two" + i}
+        variants={variants}
+        style={{
+          maxWidth: Math.random() * (100 - 40) + 40 + "%",
+        }}
+        className="flex flex-row rounded-full border border-neutral-100 dark:border-white/[0.2] p-2  items-center space-x-2 bg-neutral-100 dark:bg-black w-full h-4"
+      ></motion.div>
+    ))}
+  </motion.div>
   );
 };
 
