e681cce072a364108aabe6cc9487a4620cb304d4
Moiwa <samuelmoiwa@gmail.com>
2025-01-20 11:08:33 +0000

    update

--- src/pages/TrainingWorkshops.jsx
+++ src/pages/TrainingWorkshops.jsx
@@ -5,14 +5,14 @@ import TrainingAndWorkshopsComponent from '../components/services/TrainingWorksh
 const TrainingWorkshops = () => {
   return (
     <>
-    <div className='bg-gradient-to-b from-blue-50 to-green-200 w-screen h-full flex justify-center items-center flex-col'>
-        <HeroSection/>
-      <div className=' w-full py-14 flex flex-col justify-center
-        items-center mt-5 mb-10'
-      >
-        <TrainingAndWorkshopsComponent/>
+      <div className='bg-gradient-to-b from-blue-50 to-green-200 w-screen h-full flex justify-center items-center flex-col'>
+          <HeroSection/>
+        <div className=' w-full py-14 flex flex-col justify-center
+          items-center mt-5 mb-10'
+        >
+          <TrainingAndWorkshopsComponent/>
+        </div>
       </div>
-    </div>
     </>
   )
 }
