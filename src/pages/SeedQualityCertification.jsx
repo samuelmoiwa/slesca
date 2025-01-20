3d789641e4c3b6f247ea6229ecf55ab372ac644b
Moiwa <samuelmoiwa@gmail.com>
2025-01-20 11:10:35 +0000

    update

--- src/pages/SeedQualityCertification.jsx
+++ src/pages/SeedQualityCertification.jsx
@@ -5,14 +5,14 @@ import SeedQuality from '../components/services/SeedQualityCertification/SeedQua
 const SeedQualityCertification = () => {
   return (
     <>
-    <div className='bg-gradient-to-b from-blue-50 to-green-200 w-screen h-full flex justify-center items-center flex-col'>
-        <HeroSection/>
-      <div className=' w-full py-14 flex flex-col justify-center
-        items-center mt-5 mb-10'
-      >
-        <SeedQuality/>
+      <div className='bg-gradient-to-b from-blue-50 to-green-200 w-screen h-full flex justify-center items-center flex-col'>
+          <HeroSection/>
+        <div className=' w-full py-14 flex flex-col justify-center
+          items-center mt-5 mb-10'
+        >
+          <SeedQuality/>
+        </div>
       </div>
-    </div>
     </>
   )
 }
