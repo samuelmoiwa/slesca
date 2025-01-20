959c4dcf664a8d24dc0f8187fb92b7b1d1f51a4d
Moiwa <samuelmoiwa@gmail.com>
2025-01-20 11:07:32 +0000

    update

--- src/pages/AdvocacyCollaboration.jsx
+++ src/pages/AdvocacyCollaboration.jsx
@@ -6,15 +6,15 @@ import RecentProjects from '../components/home/RecentProjects'
 const AdvocacyCollaboration = () => {
   return (
     <>
-    <div className='bg-gradient-to-b from-blue-50 to-green-200 w-screen h-full flex justify-center items-center flex-col'>
-        <HeroSection/>
-      <div className=' w-full py-14 flex flex-col justify-center
-        items-center mt-5 mb-10'
-      >
-        <AdvocacyAndCollaboration/>
+      <div className='bg-gradient-to-b from-blue-50 to-green-200 w-screen h-full flex justify-center items-center flex-col'>
+          <HeroSection/>
+        <div className=' w-full py-14 flex flex-col justify-center
+          items-center mt-5 mb-10'
+        >
+          <AdvocacyAndCollaboration/>
+        </div>
       </div>
-    </div>
-    <RecentProjects/>
+      <RecentProjects/>
     </>
   )
 }
