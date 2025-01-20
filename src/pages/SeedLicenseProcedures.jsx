0cbaba7f3dbac7779ab94c5e6493f1d87a735f6f
Moiwa <samuelmoiwa@gmail.com>
2025-01-20 11:10:56 +0000

    update

--- src/pages/SeedLicenseProcedures.jsx
+++ src/pages/SeedLicenseProcedures.jsx
@@ -4,15 +4,15 @@ import LicenseProcedures from '../components/about/seedLicenseProcedures/License
 const SeedLicenseProcedures = () => {
   return (
     <>
-    <div className='bg-gradient-to-b from-blue-50 to-green-200 w-screen h-full flex justify-center items-center flex-col'>
-        <HeroSection/>
-      <div className=' w-full py-14 flex flex-col justify-center
-        items-center mt-5 mb-10'
-      >
-        <LicenseProcedures/>
+      <div className='bg-gradient-to-b from-blue-50 to-green-200 w-screen h-full flex justify-center items-center flex-col'>
+          <HeroSection/>
+        <div className=' w-full py-14 flex flex-col justify-center
+          items-center mt-5 mb-10'
+        >
+          <LicenseProcedures/>
+        </div>
       </div>
-    </div>
-  </>
+    </>
   )
 }
 
