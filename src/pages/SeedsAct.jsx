fe13426cd072149dbcf1d29d91e8c3ad2a6cadc1
Moiwa <samuelmoiwa@gmail.com>
2025-01-20 11:10:01 +0000

    update

--- src/pages/SeedsAct.jsx
+++ src/pages/SeedsAct.jsx
@@ -4,14 +4,14 @@ import SeedsActComponent from '../components/about/seedsAct/SeedsActComponent'
 const SeedsAct = () => {
   return (
     <>
-    <div className='bg-gradient-to-b from-blue-50 to-green-200 w-screen h-full flex justify-center items-center flex-col'>
-        <HeroSection/>
-      <div className=' w-full py-14 flex flex-col justify-center
-        items-center mt-5 mb-10'
-      >
-        <SeedsActComponent/>
+      <div className='bg-gradient-to-b from-blue-50 to-green-200 w-screen h-full flex justify-center items-center flex-col'>
+          <HeroSection/>
+        <div className=' w-full py-14 flex flex-col justify-center
+          items-center mt-5 mb-10'
+        >
+          <SeedsActComponent/>
+        </div>
       </div>
-    </div>
     </>
   )
 }
