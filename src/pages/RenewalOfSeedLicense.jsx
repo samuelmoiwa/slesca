3c42361678f2d1106fe3b76d2e9f7fdee3760c70
Moiwa <samuelmoiwa@gmail.com>
2025-01-20 11:11:17 +0000

    update

--- src/pages/RenewalOfSeedLicense.jsx
+++ src/pages/RenewalOfSeedLicense.jsx
@@ -5,15 +5,15 @@ import RenewalSeedLicense from '../components/about/renewalSeedLicense/RenewalSe
 const RenewalOfSeedLicense = () => {
   return (
     <>
-    <div className='bg-gradient-to-b from-blue-50 to-green-200 w-screen h-full flex justify-center items-center flex-col'>
-        <HeroSection/>
-      <div className=' w-full py-14 flex flex-col justify-center
-        items-center mt-5 mb-10'
-      >
-        <RenewalSeedLicense/>
+      <div className='bg-gradient-to-b from-blue-50 to-green-200 w-screen h-full flex justify-center items-center flex-col'>
+          <HeroSection/>
+        <div className=' w-full py-14 flex flex-col justify-center
+          items-center mt-5 mb-10'
+        >
+          <RenewalSeedLicense/>
+        </div>
       </div>
-    </div>
-  </>
+    </>
   )
 }
 
