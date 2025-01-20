e8abe7063bba9457a552307992b1813c6e6cff5f
Moiwa <samuelmoiwa@gmail.com>
2025-01-20 11:09:17 +0000

    update

--- src/pages/SLeSCAStructure.jsx
+++ src/pages/SLeSCAStructure.jsx
@@ -5,14 +5,14 @@ import SLeSCAStructureComponent from '../components/about/SLeSCAStructure/SLeSCA
 const SLeSCAStructure = () => {
   return (
     <>
-    <div className='bg-gradient-to-b from-blue-50 to-green-200 w-screen h-full flex justify-center items-center flex-col'>
-        <HeroSection/>
-      <div className=' w-full py-14 flex flex-col justify-center
-        items-center mt-5 mb-10'
-      >
-        <SLeSCAStructureComponent/>
+      <div className='bg-gradient-to-b from-blue-50 to-green-200 w-screen h-full flex justify-center items-center flex-col'>
+          <HeroSection/>
+        <div className=' w-full py-14 flex flex-col justify-center
+          items-center mt-5 mb-10'
+        >
+          <SLeSCAStructureComponent/>
+        </div>
       </div>
-    </div>
     </>
   )
 }
