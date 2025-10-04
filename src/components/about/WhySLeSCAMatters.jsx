d691fb872408d2b16f8826ead92062c9ce0a753e
Moiwa <samuelmoiwa@gmail.com>
2025-10-04 20:55:54 +0000

    update

--- src/components/about/WhySLeSCAMatters.jsx
+++ src/components/about/WhySLeSCAMatters.jsx
@@ -37,7 +37,8 @@ const WhySLeSCAMatters = () => {
                 className="flex items-start space-x-4"
               >
                 <div className="flex-shrink-0">
-                  <div className="w-10 h-10 flex items-center justify-center bg-green-100 text-green-600 rounded-full">
+                  <div className="w-10 h-10 flex items-center justify-center
+                  bg-green-100 text-green-600 rounded-full">
                     <span className="text-xl font-bold">✓</span>
                   </div>
                 </div>
